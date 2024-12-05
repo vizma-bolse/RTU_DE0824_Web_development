5e7050d764cafe7b4677d42639dac99e5f8a68bd
Vizma_Bolse <vizma.bolse@gmail.com>
2024-12-05 19:21:29 +0200

    Created folder for HW3

---
+++ TRAINING/scripts/day13.js
@@ -0,0 +1,7 @@
+// Javascript arrays = for storing multiple values in a single variable
+console.log('Arrays');
+
+const cars = ["BMW", "Audi"];
+console.log(cars);
+
+console.log("First Car", cars[0]);
